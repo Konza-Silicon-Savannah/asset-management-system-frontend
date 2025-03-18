2ab589d22b1d20b2491e3f37187c8d31915cff80
guledabdilatif <guledabdilatif@gmail.com>
2025-03-18 12:03:06 -0700

    implement dashboard

---
+++ src/screens/AssetRequest.jsx
@@ -0,0 +1,15 @@
+import { div } from 'framer-motion/client'
+import React from 'react'
+import SideBar from './partials/SideBar'
+
+const AssetRequest = () => {
+  return (
+    
+    <div style={{display:'flex'}}>
+        <SideBar/>
+        <div>AssetRequest</div>
+    </div>
+  )
+}
+
+export default AssetRequest
---
+++ src/screens/Reports.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SideBar from './partials/SideBar'
+
+const Reports = () => {
+    return (
+        <div style={{ display: 'flex' }}>
+            <SideBar />
+            <div>Reports</div>
+        </div>
+    )
+}
+
+export default Reports
