8d0b1e13acbf567adcc48e24ab7b501640c018e0
guledabdilatif <guledabdilatif@gmail.com>
2025-03-28 14:46:22 -0700

    add charts

--- src/screens/dashboard/Dashboard.jsx
+++ src/screens/dashboard/Dashboard.jsx
@@ -1,10 +1,24 @@
 
 import './dashboard.css';
+import AreaChart1 from './AreaChart1';
+import PieChart1 from './PieChart1';
+import BarChart1 from './BarChart1';
 
 const Dashboard = () => {
     return (
-        <div className="main-panel p-5">
-            Dashboard
+        <div className="main-panel py-5">
+            <div className="row">
+                <div className="col-md-8">
+                    <AreaChart1 />
+                </div>
+                <div className="col-md-4">
+                    <PieChart1 />
+                </div>
+                <div className="col-md-12">
+                    <BarChart1 />
+                </div>
+            </div>
+
         </div>
     )
 }
