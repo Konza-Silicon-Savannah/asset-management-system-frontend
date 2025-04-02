9f976bc06a018973acac3e7baca8d66450c4873f
guledabdilatif <guledabdilatif@gmail.com>
2025-04-02 13:51:59 -0700

    create top bar component

--- src/screens/dashboard/Dashboard.jsx
+++ src/screens/dashboard/Dashboard.jsx
@@ -3,10 +3,12 @@ import './dashboard.css';
 import AreaChart1 from './AreaChart1';
 import PieChart1 from './PieChart1';
 import BarChart1 from './BarChart1';
+import TopBar from './TopBar';
 
 const Dashboard = () => {
     return (
-        <div className="main-panel py-5">
+        <div className="main-panel">
+            <TopBar/>
             <div className="row">
                 <div className="col-md-8">
                     <AreaChart1 />
